5552a9f71a2efb50457a4bc772ba856f480cf546
Kevin Kobus <kevinkobus@gmail.com>
2022-10-04 14:11:11 -0600

    started rpg project

---
+++ colossal-adventure-rpg/app.js
@@ -0,0 +1,6 @@
+const readline = require("readline-sync")
+
+
+
+const userName = readline.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
--- node-practice.js
+++ node-practice.js
@@ -47,16 +47,16 @@
 // }
 // console.log('\nA value the user requested: ' + value);
 
-var readlineSync = require('readline-sync')
-readlineSync.promptCLLoop({
-    add: function(target, into) {
-      console.log(target + ' is added into ' + into + '.');
-      // Do something...
-    },
-    remove: function(target) {
-      console.log(target + ' is removed.');
-      // Do something...
-    },
-    bye: function() { return true; }
-  });
-  console.log('Exited');
+// var readlineSync = require('readline-sync')
+// readlineSync.promptCLLoop({
+//     add: function(target, into) {
+//       console.log(target + ' is added into ' + into + '.');
+//       // Do something...
+//     },
+//     remove: function(target) {
+//       console.log(target + ' is removed.');
+//       // Do something...
+//     },
+//     bye: function() { return true; }
+//   });
+//   console.log('Exited');
