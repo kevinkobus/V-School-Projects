1d12cd8511b0f373ede9e7e4b30ba2dc4900e65a
Kevin Kobus <kevinkobus@gmail.com>
2022-10-06 13:48:39 -0600

    worked on rpg

--- colossal-adventure-rpg/app.js
+++ colossal-adventure-rpg/app.js
@@ -1,6 +1,21 @@
 const readline = require("readline-sync")
 
+// Welcome console and name collection
+const opening = readline.question("Welcome to the Colossal Adventure RPG! Would you like to step back in time to the year 1985? y for yes, n for no ")
+
+if (opening === "y") {
+    const userName = readline.question("Great! May I have your name? ")
+    console.log("Hello " + userName + "!")
+}
+
+else if (opening === "n") {
+    console.log("I don't blame you, mullets and tight pants are scary!")
+}
+
+else {
+    console.log("Umm, that wasn't one of the options")
+}
 
 
-const userName = readline.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+
+
