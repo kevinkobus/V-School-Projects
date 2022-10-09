2242731b9dd8b3b1847da971f89d8a323898db70
Kevin Kobus <kevinkobus@gmail.com>
2022-10-09 12:37:10 -0600

    worked on rpg

--- colossal-adventure-rpg/app.js
+++ colossal-adventure-rpg/app.js
@@ -1,21 +1,26 @@
 const readline = require("readline-sync")
 
-// Welcome console and name collection
-const opening = readline.question("Welcome to the Colossal Adventure RPG! Would you like to step back in time to the year 1985? (Y) for Yes, (N) for No ")
+// Welcome console and name collection//
+// const opening = readline.question("Welcome to the Colossal Adventure RPG! Would you like to step back in time to the year 1985? (Y) for Yes, (N) for No ")
 
-if (opening === "y" || opening === "Y") {
-    const userName = readline.question("Great! May I have your name? ")
+function intro() {
+    let text;
+    let opening = prompt(readline.question("Welcome to the Colossal Adventure RPG! Would you like to step back in time to the year 1985? (Y) for Yes, (N) for No "))
+    if (opening === "y" || opening === "Y") {
+        text = "Great! Let's go."
+
+      else if (opening === "n" || opening === "N") {
+            text = "I don't blame you, mullets and tight pants are scary!"
+        }
+        
+        else {
+            text = "Umm, that wasn't one of the options"
+        }
+
+        document.getElementById("start").innerHTML = text
+    }
+
+
+    const userName = readline.question("May I have your name? ")
     console.log("Hello " + userName + "!")
 }
-
-else if (opening === "n" || opening === "N") {
-    console.log("I don't blame you, mullets and tight pants are scary!")
-}
-
-else {
-    console.log("Umm, that wasn't one of the options")
-}
-
-
-
-
