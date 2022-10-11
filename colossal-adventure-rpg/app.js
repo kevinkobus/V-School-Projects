42fb2f3584997176a7c515bac7007257c3e5af73
Kevin Kobus <kevinkobus@gmail.com>
2022-10-10 19:56:43 -0600

    worked on rpg with at least a little progress

--- colossal-adventure-rpg/app.js
+++ colossal-adventure-rpg/app.js
@@ -1,26 +1,29 @@
 const readline = require("readline-sync")
 
 // Welcome console and name collection//
-// const opening = readline.question("Welcome to the Colossal Adventure RPG! Would you like to step back in time to the year 1985? (Y) for Yes, (N) for No ")
 
-function intro() {
-    let text;
-    let opening = prompt(readline.question("Welcome to the Colossal Adventure RPG! Would you like to step back in time to the year 1985? (Y) for Yes, (N) for No "))
+const opening = readline.question("Welcome to 1985.  You must defeat five Hair Bands to make it to 1990.  If you'd like to continue, type: (Y) for Yes, (N) for No ")
+
     if (opening === "y" || opening === "Y") {
-        text = "Great! Let's go."
+        console.log("Great! Let's go.")
 
-      else if (opening === "n" || opening === "N") {
-            text = "I don't blame you, mullets and tight pants are scary!"
-        }
-        
-        else {
-            text = "Umm, that wasn't one of the options"
-        }
+    } else if (opening === "n" || opening === "N") {
+        console.log("I don't blame you, mullets and tight pants are scary!")
 
-        document.getElementById("start").innerHTML = text
+
+    } else {
+        console.log("Umm, that wasn't one of the options")
     }
 
 
-    const userName = readline.question("May I have your name? ")
-    console.log("Hello " + userName + "!")
+
+
+const userName = readline.question("May I have your name? ")
+console.log("Hello " + userName + "!")
+
+let player = {
+    name: userName,
+    hp: 100,
+    attackStrength: 50,
+    hairbandsDefeated: 0
 }
