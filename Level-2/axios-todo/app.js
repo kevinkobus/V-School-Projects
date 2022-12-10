fa5eaf0fc35acc49f0cf3e942ebbb568ab47df1c
Kevin Kobus <kevinkobus@gmail.com>
2022-12-10 11:13:00 -0700

    worked on styling for axios todo

--- Level-2/axios-todo/app.js
+++ Level-2/axios-todo/app.js
@@ -1,14 +1,14 @@
-// Get All and put on the DOM
+const axios = require("axios");
 
+// Get All and put on the DOM
 axios
-  .get("https://api.vschool.io/kevinkobus/todo")
+  .get("https://api.vschool.io/kevinkobus/todo/")
   .then((response) => {
-    for(let i = 0; i < response.data.length; i++){
-        const h1 = document.createElement('h1')
-        h1.textContent = response.data[i].title
-        document.form.appendChild(h1)
+    const todoList = response.data;
+    for (let i = 0; i < response.data.length; i++) {
+      const h1 = document.createElement("h1");
+      h1.textContent = todoList[i].title;
+      document.form.appendChild(h1);
     }
   })
   .catch((error) => console.log(error));
-
-  
