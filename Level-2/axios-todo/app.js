02249c7d3bf8483de47b6d0d2930e656b13651f8
Kevin Kobus <kevinkobus@gmail.com>
2022-12-10 16:49:03 -0700

    near completion of axios todo

--- Level-2/axios-todo/app.js
+++ Level-2/axios-todo/app.js
@@ -1,14 +1,29 @@
 const axios = require("axios");
 
-// Get All and put on the DOM
-axios
-  .get("https://api.vschool.io/kevinkobus/todo/")
-  .then((response) => {
-    const todoList = response.data;
-    for (let i = 0; i < response.data.length; i++) {
-      const h1 = document.createElement("h1");
-      h1.textContent = todoList[i].title;
-      document.form.appendChild(h1);
-    }
-  })
-  .catch((error) => console.log(error));
+// Function for getting todos from axios
+function getTodos() {
+axios.get("https://api.vschool.io/kevinkobus/todo")
+    .then(response => postTodos(response.data))
+    .catch(error => console.log(error))
+}
+
+getTodos()
+
+// Put all on the DOM
+function postTodos(data) {
+for (let i = 0; i < data.length; i++) {
+  const h1 = document.createElement("h1");
+  h1.textContent = data[i].title;
+  document.body.appendChild(h1);
+}
+}
+
+
+
+// var newTodo = {
+//   title: "Title here",
+//   description: "Description here",
+//   price: 0,
+//   imgUrl: "http://www.example.com/some-link-to-a-cool-photo.jpg",
+//   completed: false,
+// };
